5dedc2ef4e8a33f539a2d34f84244eb2c8631f18
FRAGG33R <aissambarchil75@gmail.com>
2023-01-07 13:24:41 -0500

    add vsftpd docker file and add its config to dockercompose

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -3,18 +3,14 @@
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 
         chown -R mysql:mysql /var/lib/mysql
-
-        # init database
         mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-
-        tfile=`mktemp`
-        if [ ! -f "$tfile" ]; then
+        temporaryFile=`mktemp`
+        if [ ! -f "$temporaryFile" ]; then
                 return 1
         fi
 fi
 
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
-
         cat << EOF > /tmp/create_db.sql
 USE mysql;
 FLUSH PRIVILEGES;
@@ -28,7 +24,6 @@ CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
 GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-        # run init.sql
-        /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-        rm -f /tmp/create_db.sql
+	/usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
+	rm -f /tmp/create_db.sql
 fi
